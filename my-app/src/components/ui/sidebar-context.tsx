d10146805e21f1cb4dd6c91e9dca3a66dc2ede40
RONNY <mboyaronny41@gmail.com>
2026-01-23 17:39:11 +0300

    frontend

--- my-app/src/components/ui/sidebar-context.tsx
+++ my-app/src/components/ui/sidebar-context.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 
+export const SIDEBAR_COOKIE_NAME = "sidebar:state";
+export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7; // 1 week
+export const SIDEBAR_WIDTH = "16rem";
+export const SIDEBAR_WIDTH_MOBILE = "18rem";
+export const SIDEBAR_WIDTH_ICON = "3rem";
+export const SIDEBAR_KEYBOARD_SHORTCUT = "b";
+
 type SidebarContext = {
   open: boolean;
   setOpen: (open: boolean) => void;
